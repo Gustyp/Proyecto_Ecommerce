2d78cd809e2569cb466a4667894fcc9e64d6f0d8
Gustavo Carro <styug96@gmail.com>
2022-02-23 21:39:07 -0300

    Entrega segundo desafío: Crea tu landing

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import './scss/App.scss';
 import NavBar from './components/NavBar';
+import ItemListContainer from './components/ItemListContainer';
 
 const App = () => {
+    const mensaje = "Este es mi ItemListContainer";
     return (
         <>
             <NavBar />
+            <ItemListContainer mensaje={mensaje} />
         </>
     )
 }
---
+++ src/components/ItemListContainer/index.jsx
@@ -0,0 +1,11 @@
+const ItemListContainer = ({mensaje}) => {
+    return (
+        <>
+            <div className="container mt-5 pt-5 text-center">
+                <h1>{mensaje}</h1>
+            </div>
+        </>
+    );
+}
+
+export default ItemListContainer;
